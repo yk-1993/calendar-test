f9c7248aa357b1bfbf991f79ee464300bb566a70
YusukeKimura <y_kimura@two-six.jp>
2023-03-30 11:45:09 +0900

    SSRサイドとクライアントサイドのClassNameを一致させる

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,56 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { CacheProvider } from "@emotion/react";
+import createEmotionServer from "@emotion/server/create-instance";
+import createCache from "@emotion/cache";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    const cache = createCache({ key: "css" });
+    const { extractCritical } = createEmotionServer(cache);
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <CacheProvider value={cache}>
+                <App {...props} />
+              </CacheProvider>
+            ),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      const styles = extractCritical(initialProps.html);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+            <style
+              data-emotion={`css ${styles.ids.join(" ")}`}
+              dangerouslySetInnerHTML={{ __html: styles.css }}
+            />
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
